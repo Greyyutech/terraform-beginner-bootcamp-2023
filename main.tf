3850dfc8daf06936a8a378213a1d94871096e2b3
Digitaltechot <52209414+Greyyutech@users.noreply.github.com>
2023-11-12 12:36:48 +0800

    #28 enabled static website hosting, uploaded index and error file via terrafrom (#29)

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 terraform {
   #backend "remote" {
   #  hostname = "app.terraform.io"
-  #  organization = "ExamPro"
+  #  organization = "cloudnoot"
 
   #  workspaces {
   #    name = "terra-house-1"
   #  }
   #}
   #cloud {
-  #  organization = "ExamPro"
+  #  organization = "cloudnoot"
   #  workspaces {
   #    name = "terra-house-1"
   #  }
@@ -20,4 +20,6 @@ module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
 }
