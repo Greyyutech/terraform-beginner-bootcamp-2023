71993a2917fb38969bd99cd68dc421fcae59b340
Digitaltechot <52209414+Greyyutech@users.noreply.github.com>
2023-11-26 23:36:34 +0800

    #34 invalidate cloudfront stack when content version chagnes (#35)

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
 output "bucket_name" {
   description = "Bucket name for our static website hosting"
-  value = module.terrahouse_aws.bucket_name
+  value       = module.terrahouse_aws.bucket_name
 }
 
 output "s3_website_enpoint" {
   description = "S3 Static Website hosting endpoint"
-  value = module.terrahouse_aws.website_enpoint
+  value       = module.terrahouse_aws.website_enpoint
+}
+
+output "cloudfront_url" {
+  description = "The CloudFront Distribution Domain Name"
+  value = module.terrahouse_aws.cloudfront_url
 }
